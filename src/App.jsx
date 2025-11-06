7687f040350267aca9240933a6dfb8635141aa02
mustafattk110505-ai <mustafattk110505@gmail.com>
2025-11-06 16:20:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import ChartPanel from './components/ChartPanel'
-import RightPanel from './components/RightPanel'
-export default function App(){
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import ChartPanel from './components/ChartPanel';
+import RightPanel from './components/RightPanel';
+import CanliSinyalTablosu from "./components/CanliSinyalTablosu";
+
+export default function App() {
   return (
     <div className="min-h-screen grid grid-cols-12 gap-4 p-4 bg-gray-900 text-white">
+      {/* Sol panel */}
       <div className="col-span-3"><Sidebar/></div>
-      <div className="col-span-6"><ChartPanel/></div>
+
+      {/* Orta panel */}
+      <div className="col-span-6">
+        <ChartPanel/>
+        {/* Canlı sinyal tablosunu buraya ekledik */}
+        <div className="mt-4">
+          <h1 className="text-2xl font-bold mb-2 text-center">Canlı Sinyaller</h1>
+          <CanliSinyalTablosu />
+        </div>
+      </div>
+
+      {/* Sağ panel */}
       <div className="col-span-3"><RightPanel/></div>
     </div>
-  )
+  );
 }
